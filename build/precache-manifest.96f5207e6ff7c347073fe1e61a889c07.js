763b9ceb7764036cfc35329943cc7d768859d9b2
Daniel Finkle <daniel.finkle1@gmail.com>
2023-05-26 17:11:30 -0700

    build for hosting

---
+++ build/precache-manifest.96f5207e6ff7c347073fe1e61a889c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fedda19c45e286ddfc7e",
+    "url": "/static/css/main.1e3d219b.chunk.css"
+  },
+  {
+    "revision": "fedda19c45e286ddfc7e",
+    "url": "/static/js/main.fedda19c.chunk.js"
+  },
+  {
+    "revision": "eb2ce4e95dc57512045f",
+    "url": "/static/js/1.eb2ce4e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e31e721386486bf29ecb2e742be4a775",
+    "url": "/index.html"
+  }
+];
